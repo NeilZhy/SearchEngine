c291adf7b662d61ed05f89da995a4064b5cb307d
neilzhy <neilzhy@163.com>
2018-06-01 11:25:49 +0800

    去标签脚本

--- script/pre_html.py
+++ script/pre_html.py
@@ -2,9 +2,11 @@
 import sys
 import os
 import re
-
+##过滤HTML中的标签
+#将HTML中标签等信息去掉
+#@param htmlstr HTML字符串.
 def filter_tags(htmlstr):
-
+    #先过滤CDATA
     re_cdata=re.compile('//<!\[CDATA\[[^>]*//\]\]>',re.I) #匹配CDATA
     re_script=re.compile('<\s*script[^>]*>[^<]*<\s*/\s*script\s*>',re.I)#Script
     re_style=re.compile('<\s*style[^>]*>[^<]*<\s*/\s*style\s*>',re.I)#style
@@ -17,32 +19,36 @@ def filter_tags(htmlstr):
     s=re_br.sub('\n',s)#将br转换为换行
     s=re_h.sub('',s) #去掉HTML 标签
     s=re_comment.sub('',s)#去掉HTML注释
-
+    #去掉多余的空行
     blank_line=re.compile('\n+')
     s=blank_line.sub('\n',s)
     s=replaceCharEntity(s)#替换实体
     return s
 
-
+##替换常用HTML字符实体.
+#使用正常的字符替换HTML中特殊的字符实体.
+#你可以添加新的实体字符到CHAR_ENTITIES中,处理更多HTML字符实体.
+#@param htmlstr HTML字符串.
 def replaceCharEntity(htmlstr):
     CHAR_ENTITIES={'nbsp':' ','160':' ',
-                   'lt':'<','60':'<',
-                   'gt':'>','62':'>',
-                   'amp':'&','38':'&',
-                   'quot':'"','34':'"',}
+	'lt':'<','60':'<',
+	'gt':'>','62':'>',
+	'amp':'&','38':'&',
+	'quot':'"','34':'"',}
 
-     re_charEntity=re.compile(r'&#?(?P<name>\w+);')
-     sz=re_charEntity.search(htmlstr)
-     while sz:
-         entity=sz.group()#entity全称，如&gt;
-         key=sz.group('name')#去除&;后entity,如&gt;为gt
-         try:
-                 htmlstr=re_charEntity.sub(CHAR_ENTITIES[key],htmlstr,1)
-                 sz=re_charEntity.search(htmlstr)
-         except KeyError:
-                 htmlstr=re_charEntity.sub('',htmlstr,1)
-                 sz=re_charEntity.search(htmlstr)
-                 return htmlstr
+    re_charEntity=re.compile(r'&#?(?P<name>\w+);')
+    sz=re_charEntity.search(htmlstr)
+    while sz:
+        entity=sz.group()#entity全称，如&gt;
+        key=sz.group('name')#去除&;后entity,如&gt;为gt
+	try:
+            htmlstr=re_charEntity.sub(CHAR_ENTITIES[key],htmlstr,1)
+            sz=re_charEntity.search(htmlstr)
+	except KeyError:
+	    #以空串代替
+            htmlstr=re_charEntity.sub('',htmlstr,1)
+            sz=re_charEntity.search(htmlstr)
+    return htmlstr
 
 def enum_dir(input_path, file_list):
     ls = os.listdir(input_path)
@@ -59,17 +65,17 @@ def enum_dir(input_path, file_list):
 def replaceAllFile(input_path, output_path):
     if input_path[-1] != '/':
         input_path = input_path + '/'
-        file_list = []
-        enum_dir(input_path, file_list)
-        for f in file_list:
-            content = file(f).read()
-            plain_content = filter_tags(content)
-            output_file = os.path.join(output_path, f[len(input_path):])
-            output_dir = os.path.dirname(output_file)
-            if not os.path.exists(output_dir):
-                os.makedirs(output_dir)
-            file(output_file, 'w').write(plain_content)
-            print 'file:' + output_file + ' done!'
+    file_list = []
+    enum_dir(input_path, file_list)
+    for f in file_list:
+        content = file(f).read()
+        plain_content = filter_tags(content)
+        output_file = os.path.join(output_path, f[len(input_path):])
+        output_dir = os.path.dirname(output_file)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        file(output_file, 'w').write(plain_content)
+        print 'file:' + output_file + ' done!'
 
 if __name__=='__main__':
     if len(sys.argv) != 3:
